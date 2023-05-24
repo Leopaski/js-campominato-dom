a913c0ea0f8975a8d07c32cce832bad21846e35e
Leopaski <leonardo.pascale@gmail.com>
2023-05-24 22:06:46 +0200

    try to add number at cell

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,17 @@
 const tabella = document.getElementById('tabella');
-function createCell(className){
-    const cell=document.createElement('div'); 
-   	cell.classList.add(className); 
-    return cell;
+function createCell(className,htmlContent){
+    const htmlElement=document.createElement('div'); 
+   	htmlElement.classList.add(className); 
+    htmlElement.innerHTML = htmlContent;
+    return htmlElement;
 }
 const play=document.getElementById('play');
 play.addEventListener('click',function(){
     tabella.innerHTML="";
     for (i=0;i<100;i++){
         tabella.appendChild(createCell("cella"));
-        const cellElement=document.querySelector('.cella')
-cellElement.addEventListener('click',function(){
+        const cellElement=document.querySelector('.cella', `<p>${i+1}</p>`)
+        cellElement.addEventListener('click',function(){
 
 cellElement.classList.toggle('clicked') } 
 )
